3ad26fe8dedcd4bf690917708315666212950133
thomas.baric@epitech.eu <thomas.baric@epitech.eu>
2019-02-22 17:14:02 +0100

    coucou

--- script/draw.js
+++ script/draw.js
@@ -1,20 +1,21 @@
 $(document).ready(function () {
 
-    var path = document.querySelectorAll('path'),  percentScroll;
+
+var path = document.querySelectorAll('path'), 
+		percentScroll;
 
 $(path).each(function(){
-this.style.strokeDasharray = this.getTotalLength();
-this.style.strokeDashoffset = this.getTotalLength();
+  this.style.strokeDasharray = this.getTotalLength();
+  this.style.strokeDashoffset = this.getTotalLength();
 });
 
 window.onscroll = function(){
-percentScroll = window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-
-$(path).each(function(){
-this.style.strokeDashoffset = Math.floor(this.getTotalLength() * (1 - percentScroll));
-});
+  percentScroll = (window.pageYOffset / (document.body.offsetHeight - window.innerHeight)) / 13;
+	console.log(percentScroll);
+  $(path).each(function(){
+    this.style.strokeDashoffset = Math.floor(this.getTotalLength() * (1 - percentScroll));
+  });
 };
-
 
 });
 
