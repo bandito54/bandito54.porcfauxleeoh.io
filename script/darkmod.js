0844b43439d886e4b1138beedcf721a33db69260
thomas.baric@epitech.eu <thomas.baric@epitech.eu>
2019-02-25 12:11:51 +0100

    darkmod

--- script/darkmod.js
+++ script/darkmod.js
@@ -1,11 +1,11 @@
 function swap_mod() {
     if (document.getElementById('darkornot').checked)
     {
-        document.getElementById("style_dl").href="../style/style_dark.css"; 
+        document.getElementById("style_dl").href="./style/style_dark.css"; 
         console.log("checked");
     }
     else {
-        document.getElementById("style_dl").href="../style/style.css"; 
+        document.getElementById("style_dl").href="./style/style.css"; 
         console.log("not checked");
     }
 }
